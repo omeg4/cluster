52a4ac44eb0ec01ef25b455439d02f0aecc3a7c4
omeg4 <matthew.brunetti28@gmail.com>
2018-05-17 18:32:41 -0500

    removing old data

--- sigesn/results/DIR-si-2018-05-17-RF-test/fin-evtab-min.m
+++
@@ -1,2 +0,0 @@
-(* Created with the Wolfram Language : www.wolfram.com *)
-{{1[[1]][[3]]}, {1[[2]]}}
--- sigesn/results/IND-si-2018-05-17-RF-ind-test/callfuncs.m
+++
@@ -1,6 +0,0 @@
-params={1.9,0.046,650000,0.4,11.9};
-etab={0,2.5,0.5};
-dtab={1,5,};
-Export["diag1.txt","Params, Dtab, and Etab initialized"]
-SGSIndSuite[params,4.89,etab,dtab,IndCoul];
-Quit[]
