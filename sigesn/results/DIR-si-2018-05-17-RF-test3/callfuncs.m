caa5c6310200fb48df28113b98035bea6d64ce1d
omeg4 <matthew.brunetti28@gmail.com>
2018-05-17 23:50:38 -0500

    first successful RF run

---
+++ sigesn/results/DIR-si-2018-05-17-RF-test3/callfuncs.m
@@ -0,0 +1,7 @@
+SetDirectory["/home/mbrunetti/cluster/sigesn/results/DIR-si-2018-05-17-RF-test3"]
+params={1.9,0.046,650000,0.4,11.9};
+etab={0,2.5,0.5};
+Export["diag1.txt","Params and Etab initialized"]
+SiGeSuite[3,params,etab,4.89];
+Export["suitediag.txt","Suite run complete"]
+Quit[]
