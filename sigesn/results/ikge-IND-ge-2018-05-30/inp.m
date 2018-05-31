d83af38f0518abca761eb3183504b265aa18b024
omeg4 <matthew.brunetti28@gmail.com>
2018-05-31 18:07:11 -0500

    adding ik runs

---
+++ sigesn/results/ikge-IND-ge-2018-05-30/inp.m
@@ -0,0 +1,2 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+{{16.5, 0.0676, 620000, 0.45, 16}, 4.89, {0, 2.75, 0.25}, {1, 5, 1}}
---
+++ sigesn/results/iksi-IND-si-2018-05-30/inp.m
@@ -0,0 +1,2 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+{{0.95, 0.046, 650000, 0.4, 11.9}, 4.89, {0, 2.75, 0.25}, {1, 5, 1}}
