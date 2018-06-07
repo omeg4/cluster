48f2c24ceb2eef8db9c6155d8e0bd4671683114c
omeg4 <matthew.brunetti28@gmail.com>
2018-06-06 20:37:15 -0500

    reprocs and norms

--- sigesn/results/fsge-DIR-ge-2018-06-05/callfuncs.m
+++ sigesn/results/fsge-DIR-ge-2018-06-05/callfuncs.m
@@ -1,10 +1,5 @@
 SetDirectory["/home/mbrunetti/cluster/sigesn/results/fsge-DIR-ge-2018-06-05"]
-params={16.5,0.0676,620000,0.45,16};
-etab={0,2.7,0.1};
-Export["inp.m",{params,1,etab,{0,0,1}}]
-Export["diag1.txt","Params and Etab initialized"]
-Export["suite.m",SiGeSuite[3,params,etab,1]];
-labels={"min","max"};
-Export["suitediag.txt","Suite run complete, beginning analysis of data files."]
 Export["proc.m",ProcessSuite[]];
+"Reproc complete. Checking normalization.">>>"diag.txt"
+checksuitenorm[Import["suite.m"]];
 Quit[]
--- sigesn/results/fssn-DIR-sn-2018-06-05/callfuncs.m
+++ sigesn/results/fssn-DIR-sn-2018-06-05/callfuncs.m
@@ -1,10 +1,5 @@
 SetDirectory["/home/mbrunetti/cluster/sigesn/results/fssn-DIR-sn-2018-06-05"]
-params={50.5,0.085,550000,0.5,24};
-etab={0,2.7,0.1};
-Export["inp.m",{params,1,etab,{0,0,1}}]
-Export["diag1.txt","Params and Etab initialized"]
-Export["suite.m",SiGeSuite[3,params,etab,1]];
-labels={"min","max"};
-Export["suitediag.txt","Suite run complete, beginning analysis of data files."]
 Export["proc.m",ProcessSuite[]];
+"Reproc complete. Checking normalization.">>>"diag.txt"
+checksuitenorm[Import["suite.m"]];
 Quit[]
