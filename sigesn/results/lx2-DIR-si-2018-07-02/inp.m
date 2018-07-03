f08fce4d4ece3614761f1c1fc54ff561c9362ec0
omeg4 <matthew.brunetti28@gmail.com>
2018-07-03 12:54:13 -0500

    adding lx data

---
+++ sigesn/results/lx2-DIR-si-2018-07-02/inp.m
@@ -0,0 +1,2 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+{{0.95, 0.046, 650000, 0.325, 11.9}, 1, {0, 2, 0.5}, {0, 0, 1}}
---
+++ sigesn/results/lx5-DIR-lx5-2018-07-02/callfuncs.m
@@ -0,0 +1,12 @@
+SetDirectory["/home/mbrunetti/cluster/sigesn/results/lx5-DIR-lx5-2018-07-02"]
+params={0.95,0.046,650000,0.4,11.9};
+etab={0,2,0.5};
+Export["inp.m",{params,1,etab,{0,0,1}}]
+"Inputs saved. Initializing suite.">>>"diag.txt"
+Export["suite.m",SiGeSuite[3,params,etab,1]];
+"Suite run complete. Processing data.">>>"diag.txt"
+labels={"min","max"};
+Export["proc.m",ProcessSuite[]];
+"Processing complete. Checking normalization.">>>"diag.txt"
+checksuitenorm[Import["suite.m"]];
+Quit[]
