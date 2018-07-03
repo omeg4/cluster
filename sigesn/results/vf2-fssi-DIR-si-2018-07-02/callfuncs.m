bb64f81c675c0b87bc31e823fb7c4036e3850b3b
omeg4 <matthew.brunetti28@gmail.com>
2018-07-03 10:44:35 -0500

    adding new vf results

---
+++ sigesn/results/vf2-fssi-DIR-si-2018-07-02/callfuncs.m
@@ -0,0 +1,12 @@
+SetDirectory["/home/mbrunetti/cluster/sigesn/results/vf2-fssi-DIR-si-2018-07-02"]
+params={0.95,0.046,525000,0.4,11.9};
+etab={0,2,0.5};
+Export["inp.m",{params,1,etab,{0,0,1}}]
+"Inputs saved. Initializing suite.">>>"diag.txt"
+Export["suite.m",SiGeSuite[3,params,etab,1]];
+"Suite run complete. Processing data.">>>"diag.txt"
+labels={"min","max"};
+Export["proc.m",ProcessSuite[]];
+"Processing complete. Checking normalization.">>>"diag.txt"
+checksuitenorm[Import["suite.m"]];
+Quit[]
---
+++ sigesn/results/vf2-fssi-DIR-si-2018-07-02/inp.m
@@ -0,0 +1,2 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+{{0.95, 0.046, 525000, 0.4, 11.9}, 1, {0, 2, 0.5}, {0, 0, 1}}
