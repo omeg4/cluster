ac7212a95b2c5d6060504bfca2953bef10e3b608
omeg4 <matthew.brunetti28@gmail.com>
2018-07-05 17:09:34 -0400

    removed bad results

--- tmdc/results/A-emo-hw-hbn-IND-2018-07-04/callfuncs.m
+++
@@ -1,7 +0,0 @@
-SetDirectory["/home/mbrunetti/cluster/tmdc/results/A-emo-hw-hbn-IND-2018-07-04"]
-{me,mh,chi2d,kappa,d}=Import["../../inputs.m"]
-Export["inp.m",{me,mh,chi2d,kappa}];
-"Inputs saved. Initializing suite.">>>"diag.txt"
-Export["suite.m",IndKeld[3,{me,mh,chi2d},kappa,d]
-Export["suite.m",suite];
-Quit[]
