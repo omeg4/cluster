d49efac82f09d432937277bd8aa3761e90eebae6
omeg4 <matthew.brunetti28@gmail.com>
2018-07-05 18:17:49 -0500

    adding tmdc results

---
+++ tmdc/results/A-ew-hm-hbn-IND-2018-07-05/callfuncs.m
@@ -0,0 +1,6 @@
+SetDirectory["/home/mbrunetti/cluster/tmdc/results/A-ew-hm-hbn-IND-2018-07-05"]
+{me,mh,chi2d,kappa,d}=Import["../../inputs.m"]
+Export["inp.m",{me,mh,chi2d,kappa, d}];
+"Inputs saved. Initializing suite.">>>"diag.txt"
+Export["suite.m",IndKeld[3,{me,mh,chi2d},kappa,d]];
+Quit[]
