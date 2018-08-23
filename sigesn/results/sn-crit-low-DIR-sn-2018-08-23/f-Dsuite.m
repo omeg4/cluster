73973a6ff099d630490e7162b9ba6d7902146f1a
omeg4 <matthew.brunetti28@gmail.com>
2018-08-23 14:26:41 -0500

    sn critical

---
+++ sigesn/results/sn-crit-low-DIR-sn-2018-08-23/f-Dsuite.m
@@ -0,0 +1,26 @@
+SiGeSuite[mmax_, matpars_, eztab_, kappa_] :=
+  Module[
+   {
+    (* Unpack material parameters and get units straight *)
+    Egap = matpars[[1]],
+    (* Express bandgaps in Joules *)
+    d0 = matpars[[2]]*Unm/Um,
+	(* Express buckling constant in meters *)
+    vF = matpars[[3]],
+	(* Fermi velocity is already in m/s *)
+    ds = matpars[[4]]*Unm/Um, (* Monolayer in thickness in meters *)
+	erel = matpars[[5]] - 1,
+    \[Kappa] = kappa,
+	min,
+	max
+    },
+	{
+		Table[
+		{
+			DirKeld[mmax, matpars[[2 ;;]], Egap, Eperp, \[Kappa], 1],
+			DirKeld[mmax, matpars[[2 ;;]], Egap, Eperp, \[Kappa], -1]
+		},
+		{Eperp, eztab[[1]], eztab[[2]], eztab[[3]]}
+		]
+	}
+]
---
+++ sigesn/results/sn-crit-low-DIR-sn-2018-08-23/inp.m
@@ -0,0 +1,2 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+{{50.5, 0.085, 550000, 0.5, 24}, 1, {0.58, 0.62, 0.02}, {0, 0, 1}}
