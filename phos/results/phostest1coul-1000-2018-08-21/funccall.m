acc88e5632b9817490f76bfe183e3fa8ed3250d4
omeg4 <matthew.brunetti28@gmail.com>
2018-08-29 20:54:11 -0500

    wiping bad results

--- phos/results/phostest1coul-1000-2018-08-21/funccall.m
+++
@@ -1,5 +0,0 @@
-SetDirectory["/home/mbrunetti/cluster/phos/results/phostest1coul-1000-2018-08-21"]
-Export["inps.txt",{phostest1coul,"C",1,1,1,1,0,1000}];
-result=CompPhos[4,{1,1,1,1},0,"C",1000];
-Export["suite.m",result]
-Quit[]
