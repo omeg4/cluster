27abf667fb789ff0b6bf3ce8a1548152835f0666
omeg4 <matthew.brunetti28@gmail.com>
2018-09-23 22:23:23 -0400

    uploading new phosfuncs for cluster test

--- phos/phosfuncs.m
+++ phos/phosfuncs.m
@@ -1,7 +1,13 @@
+(* ::Package:: *)
+
 (* Set up the NDEigensystem function wrapper *)
 Coulomb[k_,d_][x_,y_]:= -1 / (k*Sqrt[x^2 + y^2 + d^2])
 
 Keldysh[k_,d_,chi_][x_,y_] := - (Pi / (chi)) * (StruveH[0,Sqrt[x^2 + y^2 + d^2]/(chi / (2 * k))] - BesselY[0,Sqrt[x^2 + y^2 + d^2]/(chi / (2 * k))])
+
+Harmosc[mx_,my_,eps_,d_,v0_][x_,y_]:= (1/(2*eps*(d^3)))*(x^2+y^2) - (1/(eps*d))
+
+psi1d[a_][n_][x_]:=Sqrt[1/(Sqrt[Pi]*a)]*1/Sqrt[(2^n)*Factorial[n]]*Exp[-(x^2)/(2*a)]*HermiteH[n,x/a]
 
 V[pot_] := If[pot=="K",
 	Keldysh[kappa,d,chi],
@@ -13,7 +19,7 @@ omega[reg_] := If[reg=="D",
 	Rectangle[{-s,-s},{s,s}]
 ]
 
-CompPhos[nmax_,params_,dee_,pot_,reg_,s_,ns_]:=Module[
+CompPhos[nmax_,params_,dee_,pot_]:=Module[
 	{
 		mx = params[[1]],
 		my = params[[2]],
@@ -23,20 +29,26 @@ CompPhos[nmax_,params_,dee_,pot_,reg_,s_,ns_]:=Module[
 		d = dee,
 		ev,
 		ef,
-		norms
+		norms,
+		tds,
+		tdstrans,
+		eps=10^-3,
+		MC=10^-4
 	},
+	tds=- (1/2) * ( (1/mx)*D[f[x,y],{x,2},{y,0}] + (1/my)*D[f[x,y],{x,0},{y,2}]) + pot[x,y] * f[x,y] + shift*f[x,y];
+	tdstrans=Simplify[tds/.{f->(u[ArcTan[#1],ArcTan[#2]]&)}/.{x->(Tan[\[Xi]]),y->(Tan[\[Psi]])},{(Pi/2)>\[Xi]>-(Pi/2),(Pi/2)>\[Psi]>-(Pi/2)}];
 	{ev, ef} = NDEigensystem[
 			{
-				- (1/2) * ( (1/mx)*D[f[x,y],{x,2},{y,0}] + (1/my)*D[f[x,y],{x,0},{y,2}]) + pot[x,y] * f[x,y] + shift*f[x,y],
-				DirichletCondition[f[x,y] == 0, True]
+				tdstrans,
+				DirichletCondition[u[\[Xi],\[Psi]]==0,Abs[\[Xi]]==(Pi/2-eps)||Abs[\[Psi]]==(Pi/2-eps)]
 			},
-			f[x,y],
-			{x,y} \[Element] reg,
+			u[\[Xi],\[Psi]],
+			{\[Xi],\[Psi]}\[Element]Rectangle[{-Pi/2+eps,-Pi/2+eps},{Pi/2-eps,Pi/2-eps}],
 			nmax,
-			Method->{"SpatialDiscretization"->{"FiniteElement",{"MeshOptions"->{"MaxCellMeasure"->s/ns}}},"Eigensystem"->{"Arnoldi",MaxIterations->10^8}}
+			Method->{"SpatialDiscretization"->{"FiniteElement",{"MeshOptions"->{"MaxCellMeasure"->MC}}},"Eigensystem"->{"Arnoldi",MaxIterations->10^5}}
 	];
 	{
-		{mx,my,chi,kappa,d,s,ns},
+		{mx,my,chi,kappa,d},
 		{UnitConvert[Quantity[ev - shift,"Hartrees"],"Millielectronvolts"], ef}
 	}
 ]
