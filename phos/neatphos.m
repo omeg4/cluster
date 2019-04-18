97ed4a58fbea081f590424cae1fddcbdf3b82044
omeg4 <matthew.brunetti28@gmail.com>
2019-04-17 22:55:01 -0400

    meant to update phos thickness from 0.85 nm to 0.541 nm

--- phos/neatphos.m
+++ phos/neatphos.m
@@ -15,7 +15,7 @@ H2eV = 27.21138602;
 na = (5 * 10^15) * (10^-18);
 damp = (10^13) * (T2S);
 lBN = (1 / B2nm) * 0.333;
-lphos = (1 / B2nm) * 0.85;
+lphos = (1 / B2nm) * 0.541;
 
 
 (* Set up phosphorene parameters *)
